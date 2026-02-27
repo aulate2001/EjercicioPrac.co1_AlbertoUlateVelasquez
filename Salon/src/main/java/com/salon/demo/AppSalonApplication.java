98c772f0542ba51637de21511d6b0f26182a4003
aulate2001 <albertoulate113@gmail.com>
2026-02-26 18:25:03 -0600

    Proyecto Creado

---
+++ Salon/src/main/java/com/salon/demo/AppSalonApplication.java
@@ -0,0 +1,13 @@
+package com.salon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSalonApplication.class, args);
+	}
+
+}
